e48724ae6a54df88ed6793e6fe569fe29811c7b1
Tharindu-Gimhana <167947975+Tharindu-Gimhana@users.noreply.github.com>
2025-11-22 18:40:58 +0530

    change base image

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: "/",
+  base: "/My-Portfolio/",
   plugins: [react()],
   esbuild: {
     jsx: "automatic",
