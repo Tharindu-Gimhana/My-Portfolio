5762f72788276d2286c3bd7448dbc06e8c0a48f2
Tharindu-Gimhana <167947975+Tharindu-Gimhana@users.noreply.github.com>
2025-11-22 17:53:45 +0530

    add base

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,10 @@
-export default {
-  plugins: [],
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
+export default defineConfig({
+  base: "/",
+  plugins: [react()],
   esbuild: {
-    jsx: 'automatic',
+    jsx: "automatic",
   },
-}
+});
